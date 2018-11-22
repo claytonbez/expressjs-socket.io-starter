50f4c7960a87d88d876dbb0a2ff33f24fd76e48f
Clayton Bezuidenhout <bezclayton@gmail.com>
2018-11-22 10:21:47 +0200

    Update server.js
    Made major changes, based on personal preferences

--- server.js
+++ server.js
@@ -1,30 +1,65 @@
-//ExpressJS-Socket.io starter
-var fs = require('fs'); //to read certificates from fileSystem
-var https = require('https');
+//--------------------------------------------------------------------------------
+// GLOBAL CONTANTS
+//--------------------------------------------------------------------------------
+const FMT = "YYYY-MM-DD HH:mm:ss";
+//--------------------------------------------------------------------------------
+// REQUIREMENTS
+//--------------------------------------------------------------------------------
+var program = require('commander');
+var fs = require('fs-extra');
+var path = require('path');
+var moment = require('moment');
+var colors = require('colors');
 var express = require('express');
-var app = express();
-var options = {key: fs.readFileSync('./coltron.key'), cert: fs.readFileSync('./coltron.cert')};
-var serverPort = 443; //Default https port
-var server = https.createServer(options, app); // construct server 
-var io = require('socket.io')(server); //Attach Socket.io to http server
-//Express JS Setup
-app.use("/", express.static(__dirname + "/public")); // ALL STATIC REQUESTS TO BE ROUTED TO /public
+var app = express(); 
+var http = require('http');
+var server = http.Server(app);
+var io = require('socket.io')(server);
+//--------------------------------------------------------------------------------
+// COMMANDER
+//--------------------------------------------------------------------------------
+program.version('0.0.1')
+    .option('-p, --port <string>', 'Server Port to listen on',80)
+    .option('-f, --folder <path>', 'Public host dir', path.join(__dirname,'/public'))
+    .parse(process.argv);
+
+program.folder = (program.folder[0] == "/" ? program.folder : path.join(__dirname,program.path));
+
+//--------------------------------------------------------------------------------
+// EXPRESS ROUTES
+//--------------------------------------------------------------------------------
+
+app.use(function (req, res, next) {
+    console.log('~$ ' + req.originalUrl);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
+
+app.use("/", express.static(program.folder));
+
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + '/index.html'); //FIRST FILE TO BE SERVED IS index.html IN THE /public FOLDER
+    res.sendFile(path.join(program.folder,'index.html'));
 });
-app.get("/downloads", function (req, res) { //DOWNLOADS ARE NOT PLACED IN /public FOLDER and can only be accessed via this Route
-    var fname = "./downloads/" + req.query['file'];// 'file' is set in name= tag of html input
-    res.download(fname);
+
+//--------------------------------------------------------------------------------
+// SOCKET.IO
+//--------------------------------------------------------------------------------
+
+io.on('connection',(socket) => {
+        console.log(colors.magenta.bold(`[C]    ${socket.id}`));
+
+        socket.on('disconnect',()=>{
+            console.log(colors.yellow.bold(`[D]     ${socket.id}`));
+        });
 });
-//
-io.on('connection',function(socket){
-		// the socket object can be used within the connection only. coding applies to each socket that connects individually.
-		console.log(socket.id); 
-		socket.on('disconnect',function(){
-			console.log('Disconnected :' + socket.id) ; 
-		})
-});
-//Server Start
-server.listen(serverPort, function() {
-    console.log('server up and running at %s port', serverPort);
+
+//--------------------------------------------------------------------------------
+// SERVER INITIATE
+//--------------------------------------------------------------------------------
+
+server.listen(program.port,()=>{
+    console.log(colors.green.bold(`Server listening on port ${program.port}`))
 });
